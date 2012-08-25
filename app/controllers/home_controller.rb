937d19c840f06487155f945cb2504b9ca3a7a944
Nedomas <domas.bitvinskas@me.com>
2012-08-25 22:38:34 +0300

    Add error handling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,14 +7,20 @@ class HomeController < ApplicationController
   end
 
   def output
-  	input_symbols = params[:symbols].split(" ")
-  	symbols = Securities::Stock.new(input_symbols)
-		@history_data = symbols.history(:start_date => '2012-01-01', :end_date => '2012-03-01').results
+  	begin
+	  	input_symbols = params[:symbols].split(" ")
+	  	symbols = Securities::Stock.new(input_symbols)
+			@history_data = symbols.history(:start_date => '2012-01-01', :end_date => '2012-03-01').results
+
+		rescue Exception => exc
+   		logger.error("Message for the log file #{exc.message}")
+   		flash.now[:notice] = "#{exc.message}"
+   	end
 
 		respond_to do |format|
       format.html
       format.js
     end
-    
+
   end
 end
