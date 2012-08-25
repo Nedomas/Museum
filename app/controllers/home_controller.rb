8eaf4d7d5edc59388e92af65096b37aaed6d304c
Nedomas <domas.bitvinskas@me.com>
2012-08-25 22:09:42 +0300

    Add jQuery mechanism for loading output

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,20 @@
 class HomeController < ApplicationController
   def index
-  	
+  	respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def output
   	input_symbols = params[:symbols].split(" ")
   	symbols = Securities::Stock.new(input_symbols)
 		@history_data = symbols.history(:start_date => '2012-01-01', :end_date => '2012-03-01').results
+
+		respond_to do |format|
+      format.html
+      format.js
+    end
+    
   end
 end
