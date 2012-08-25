4a6646d2fc0b3b6c72c0b12bb6d790bcf571f570
Nedomas <domas.bitvinskas@me.com>
2012-08-25 15:21:52 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Museum::Application.config.secret_token = 'aa4ce4927a5c504979461d2e43a8b8e3e32bd07a230e70f9ad414cfc4f4d389ad91d5ac236e24620d78d6f9f46ea59c8881effdd56c9c638f752993af088d0da'
