545b2d1fe592d32035b7b2703170b72fa5422451
Nedomas <domas.bitvinskas@me.com>
2012-09-20 02:44:38 +0300

    Add symbol lookup information, fix for updated securities and indicators gems

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,23 +12,24 @@ class HomeController < ApplicationController
       @symbol = input_symbol
 	  	start_date = params[:start_date]
 	  	end_date = params[:end_date]
-	  	periods = params[:periods].to_sym
-			@history_data = Securities::Stock.new(:symbol => input_symbol, :start_date => start_date, :end_date => end_date, :periods => periods).output
-      type = params[:type].to_sym
+	  	type = params[:type].to_sym
+			@history_data = Securities::Stock.new(:symbol => input_symbol, :start_date => start_date, :end_date => end_date, :type => type).output
+      indicator = params[:indicator].to_sym
       variables = params[:variables].split(" ")
-      unless type == :none
-        indicator_data = Indicators::Data.new(@history_data).calc(:type => type, :params => variables)
+      @lookup = Securities::Lookup.new(input_symbol).output[0]
+      unless indicator == :none
+        indicator_data = Indicators::Data.new(@history_data).calc(:type => indicator, :params => variables)
         @indicator_output = indicator_data.output
         @indicator_abbr = indicator_data.abbr
         @indicator_params = indicator_data.params
-        if type == :bb
+        if indicator == :bb
           @indicator_name = ["middle band", "upper band", "lower band"]
-        elsif type == :macd
+        elsif indicator == :macd
           @indicator_name = ["macd line", "signal line", "macd histogram"]
-        elsif type == :sto
+        elsif indicator == :sto
           @indicator_name = ["fast %K", "slow %K", "full %D"]
         else 
-          @indicator_name = type
+          @indicator_name = indicator
         end
       end
 
