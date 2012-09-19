7b9ecee365f44d4d3eea7798973308733df6527a
Nedomas <domas.bitvinskas@me.com>
2012-09-20 02:06:18 +0300

    Fix for securities output change

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,12 +8,12 @@ class HomeController < ApplicationController
 
   def output
   	begin
-	  	input_symbols = params[:symbols].split(" ")
+	  	input_symbol = params[:symbol]
+      @symbol = input_symbol
 	  	start_date = params[:start_date]
 	  	end_date = params[:end_date]
 	  	periods = params[:periods].to_sym
-	  	symbols = Securities::Stock.new(input_symbols)
-			@history_data = symbols.history(:start_date => start_date, :end_date => end_date, :periods => periods)
+			@history_data = Securities::Stock.new(:symbol => input_symbol, :start_date => start_date, :end_date => end_date, :periods => periods).output
       type = params[:type].to_sym
       variables = params[:variables].split(" ")
       unless type == :none
@@ -34,11 +34,6 @@ class HomeController < ApplicationController
 
 		rescue Exception => exc
    		flash.now[:error] = "#{exc.message}"
-    else
-      empty_symbols = []
-      @history_data.each { |symbol, value| empty_symbols << symbol if value.empty?
-        flash.now[:warning] = "There were no results for #{empty_symbols.join(", ")}." unless empty_symbols.empty?
-      }
    	end
 
     # If symbol doesn't exist or returned no results from securities.
